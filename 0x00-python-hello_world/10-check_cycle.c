747d79297e88e29d178f1deb3ed227b8d6b62ec6
Racem-BOUCHNAK <racem.bouchn@gmail.com>
2023-11-27 23:52:16 +0100

    mandatory

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if list have cycle
+ * @list: pointer to list
+ * Return: 1 if cycle, 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+	while (fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+			if (slow == fast)
+				return (1);
+	}
+	return (0);
+}

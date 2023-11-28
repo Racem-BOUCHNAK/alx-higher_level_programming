5f633db2c8962859cc437672ffb3bf2836508fa8
Racem-BOUCHNAK <racem.bouchn@gmail.com>
2023-11-29 00:27:39 +0100

    fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i :
+    if ord(i) == 65 or ord(i) == 71:
+        print("", end="")
+    else:
         print("{:c}".format(i), end="")

9efaac21880124d34f53eb4f34695561a14a32f4
Racem-BOUCHNAK <racem.bouchn@gmail.com>
2023-12-05 01:43:05 +0100

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        if len(x) == 0:
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix) == 0:
             print()
-        for i in range(len(x)):
-            print("{:d}".format(x[i]),
-                  end="\n" if i is len(x) - 1 else " ")
+        for i in range(len(submatrix)):
+            print("{:d}".format(submatrix[i]),
+                  end="\n" if i is len(submatrix) - 1 else " ")

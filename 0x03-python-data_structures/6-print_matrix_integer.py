8b96b7b8445382b0ef7d1bc7565f0ad933718936
Racem-BOUCHNAK <racem.bouchn@gmail.com>
2023-12-05 01:18:53 +0100

    read+fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
+        if len(x) == 0:
+            print()
         for i in range(len(x)):
             print("{}".format(x[i]),
                   end="\n" if i is len(x) - 1 else " ")
